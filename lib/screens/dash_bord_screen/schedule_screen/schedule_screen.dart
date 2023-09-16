4510567a1f21be5d0de4cd24669f3fbce839037c
mashhood <mashhooduae@gmail.com>
2023-09-16 13:08:40 +0530

    firstCommit

--- lib/screens/dash_bord_screen/schedule_screen/schedule_screen.dart
+++ lib/screens/dash_bord_screen/schedule_screen/schedule_screen.dart
@@ -18,136 +18,140 @@ class ScheduleScreen extends StatelessWidget {
             getHeight(20.0),
             getTitleWithIconAppbar(),
             getHeight(20.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Container(
-                decoration:  BoxDecoration(
-                    color: Colors.grey.shade300,
-                    borderRadius: BorderRadius.all(Radius.circular(30))),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TabBar(
-                    tabs: const [
-                      Tab(text: 'Upcoming'),
-                      Tab(text: 'Past'),
-                    ],
-                    indicator: BoxDecoration(
-                      borderRadius: BorderRadius.circular(
-                          30.0), // Adjust the border radius as needed
-                      color: Colors
-                          .white, // Change the color to your desired background color
-                    ),
-                    labelColor: Colors.black,
-                    indicatorSize: TabBarIndicatorSize
-                        .tab, // Use tab size for the indicator
-                    labelPadding: EdgeInsets.symmetric(
-                        horizontal: 16.0), // Adjust padding as needed
-                  ),
-                ),
-              ),
-            ),
+            getCustomTabbar(),
             Expanded(
               child: TabBarView(
                 children: [
                   // Tab 1 content
-                  Expanded(
-                    child: ListView.separated(
-                      physics: const BouncingScrollPhysics(),
-                      itemCount: 25,
-                      separatorBuilder: (BuildContext context, int index) =>
-                          const SizedBox(),
-                      itemBuilder: (BuildContext context, int index) {
-                        return buildAppointmentCard(
-                            onTap: () => showModalBottomSheet(
-                                  context: context,
-                                  builder: (BuildContext builderContext) {
-                                    return Container(
-                                      width: double.maxFinite,
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: <Widget>[
-                                            SizedBox(
-                                                width: 80,
-                                                child: Divider(
-                                                  thickness: 3,
-                                                )),
-                                            Padding(
-                                              padding:
-                                                  const EdgeInsets.all(8.0),
-                                              child: Row(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment
-                                                        .spaceBetween,
-                                                children: [
-                                                  SizedBox(
-                                                    width: 50,
-                                                  ),
-                                                  regularBodyText(
-                                                      text: "Summary",
-                                                      fontSize: 20.0,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                  regularBodyText(
-                                                      text: "Cancel",
-                                                      fontColor:
-                                                          Colors.blueAccent),
-                                                ],
-                                              ),
-                                            ),
-                                            getDoctorListTile(),
-                                            getHeight(20.0),
-                                            regularBodyText(
-                                                text: "Doctors,advice",
-                                                fontWeight: FontWeight.bold),
-                                            getHeight(10.0),
-                                            getDoctorsAdviceWidget(
-                                                adviceText:
-                                                    "Drink four Litters of water everyday"),
-                                            getDoctorsAdviceWidget(
-                                                adviceText: "No smoking"),
-                                            getDoctorsAdviceWidget(
-                                                adviceText: "Sleep 8 hours"),
-                                            getHeight(20.0),
-                                            regularBodyText(
-                                              text: "Discharge",
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                            getHeight(10.0),
-                                            Row(
-                                              children: [
-                                                getPdfContainer(fileCount: "File 1"),
-                                                getPdfContainer(fileCount: "File 2"),
-                                              ],
-                                            )
-                                          ],
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                ));
-                      },
-                    ),
-                  ),
-
+                  getUpcomingTab(),
                   // Tab 2 content
-                  Expanded(
-                    child: ListView.separated(
-                      physics: const BouncingScrollPhysics(),
-                      itemCount: 25,
-                      separatorBuilder: (BuildContext context, int index) =>
-                          const SizedBox(),
-                      itemBuilder: (BuildContext context, int index) {
-                        return buildAppointmentCard();
-                      },
-                    ),
-                  )
+                  getPastTab()
                 ],
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget getPastTab() {
+    return Expanded(
+      child: ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        itemCount: 25,
+        separatorBuilder: (BuildContext context, int index) => const SizedBox(),
+        itemBuilder: (BuildContext context, int index) {
+          return buildAppointmentCard();
+        },
+      ),
+    );
+  }
+
+  Widget getUpcomingTab() {
+    return Expanded(
+      child: ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        itemCount: 25,
+        separatorBuilder: (BuildContext context, int index) => const SizedBox(),
+        itemBuilder: (BuildContext context, int index) {
+          return buildAppointmentCard(
+              onTap: () => showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext builderContext) {
+                      return Container(
+                        width: double.maxFinite,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              const SizedBox(
+                                  width: 80,
+                                  child: Divider(
+                                    thickness: 3,
+                                  )),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    const SizedBox(
+                                      width: 50,
+                                    ),
+                                    regularBodyText(
+                                        text: "Summary",
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold),
+                                    regularBodyText(
+                                        text: "Cancel",
+                                        fontColor: Colors.blueAccent),
+                                  ],
+                                ),
+                              ),
+                              getDoctorListTile(),
+                              getHeight(20.0),
+                              regularBodyText(
+                                  text: "Doctors,advice",
+                                  fontWeight: FontWeight.bold),
+                              getHeight(10.0),
+                              getDoctorsAdviceWidget(
+                                  adviceText:
+                                      "Drink four Litters of water everyday"),
+                              getDoctorsAdviceWidget(adviceText: "No smoking"),
+                              getDoctorsAdviceWidget(
+                                  adviceText: "Sleep 8 hours"),
+                              getHeight(20.0),
+                              regularBodyText(
+                                text: "Discharge",
+                                fontWeight: FontWeight.bold,
+                              ),
+                              getHeight(10.0),
+                              Row(
+                                children: [
+                                  getPdfContainer(fileCount: "File 1"),
+                                  getPdfContainer(fileCount: "File 2"),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ));
+        },
+      ),
+    );
+  }
+
+  ///Customized tab bar
+  Padding getCustomTabbar() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.grey.shade300,
+            borderRadius: const BorderRadius.all(Radius.circular(30))),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TabBar(
+            tabs: const [
+              Tab(text: 'Upcoming'),
+              Tab(text: 'Past'),
+            ],
+            indicator: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                  30.0), // Adjust the border radius as needed
+              color: Colors
+                  .white, // Change the color to your desired background color
+            ),
+            labelColor: Colors.black,
+            indicatorSize:
+                TabBarIndicatorSize.tab, // Use tab size for the indicator
+            labelPadding: const EdgeInsets.symmetric(
+                horizontal: 16.0), // Adjust padding as needed
+          ),
         ),
       ),
     );
@@ -173,7 +177,7 @@ class ScheduleScreen extends StatelessWidget {
                 pdfImage,
                 height: 50,
               ),
-              regularBodyText(text: fileCount??'', fontSize: 15.0),
+              regularBodyText(text: fileCount ?? '', fontSize: 15.0),
             ],
           ),
         ),
